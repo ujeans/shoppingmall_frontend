6deb717235e5f749fab03b94311eff9f5ed751c3
ujeans <dbwlsxkal95@gmail.com>
2024-05-20 23:26:50 +0900

    setting(feature/ujin/#4): global style 설정

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./style/theme";
 import Page from "./pages/Page";
+import { GlobalStyle } from "./style/GlobalStyle";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Page />
     </ThemeProvider>
   );
