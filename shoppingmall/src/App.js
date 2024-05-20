335c09cd68ba3c2c8e9cdba29c5b396f03e54c75
ujeans <dbwlsxkal95@gmail.com>
2024-05-20 22:48:28 +0900

    style: mainColor 전역으로 설정

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./style/theme";
 import Page from "./pages/Page";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Page />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ shoppingmall/src/style/theme.js
@@ -0,0 +1,3 @@
+export const theme = {
+  mainColor: "#EB4646",
+};
