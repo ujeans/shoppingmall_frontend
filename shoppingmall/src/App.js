0a7b898177b719990816de69058b8afef2e15afb
ujeans <dbwlsxkal95@gmail.com>
2024-05-21 22:33:27 +0900

    design(feature/ujin/#6): nav, footer 공통 컴포넌트로 만들기

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,13 +1,21 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./style/theme";
-import Page from "./pages/Page";
 import { GlobalStyle } from "./style/GlobalStyle";
+import { Route, Routes } from "react-router-dom";
+//components
+import AppLayout from "./components/commom/AppLayout";
+// pages
+import MyPage from "./pages/myPage/MyPage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Page />
+      <AppLayout>
+        <Routes>
+          <Route path="/settings" element={<MyPage />} />
+        </Routes>
+      </AppLayout>
     </ThemeProvider>
   );
 }
