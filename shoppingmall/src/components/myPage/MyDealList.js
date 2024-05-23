35770b7990199f1ffd3a8bf93f34fbb06ac0a174
ujeans <dbwlsxkal95@gmail.com>
2024-05-23 19:05:01 +0900

    feat(feature/ujin/#29): 마이페이지 나의 거래 리스트 아이템 페이지 링크 걸기

--- shoppingmall/src/components/myPage/MyDealList.js
+++ shoppingmall/src/components/myPage/MyDealList.js
@@ -1,25 +1,50 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 // assets
 import salesDetails from "../../assets/salesDetails.svg";
 import orderDetails from "../../assets/orderDetails.svg";
 import cart from "../../assets/cart.svg";
 
 const MyDealList = () => {
+  const navigate = useNavigate();
+
+  const navigateToPage = title => {
+    switch (title) {
+      case "판매 내역":
+        navigate("/sales-details");
+        break;
+      case "주문 내역":
+        navigate("/order-details");
+        break;
+      case "장바구니":
+        navigate("/cart");
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <Container>
       <Wrapper>
         <MyDealTitle>나의 거래</MyDealTitle>
         <MyDealBox>
           <Icon src={salesDetails}></Icon>
-          <ListTitle>판매 내역</ListTitle>
+          <ListTitle onClick={() => navigateToPage("판매 내역")}>
+            판매 내역
+          </ListTitle>
         </MyDealBox>
         <MyDealBox>
           <Icon src={orderDetails}></Icon>
-          <ListTitle>주문 내역</ListTitle>
+          <ListTitle onClick={() => navigateToPage("주문 내역")}>
+            주문 내역
+          </ListTitle>
         </MyDealBox>
         <MyDealBox>
           <Icon src={cart}></Icon>
-          <ListTitle>장바구니</ListTitle>
+          <ListTitle onClick={() => navigateToPage("장바구니")}>
+            장바구니
+          </ListTitle>
         </MyDealBox>
       </Wrapper>
     </Container>
