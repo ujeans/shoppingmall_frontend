3e2cd711b3e824fbf089f0bfce2cb26c2a39b3ad
ujeans <dbwlsxkal95@gmail.com>
2024-05-25 22:08:03 +0900

    feat(feature/ujin/#41): buy now 버튼 클릭시주문 내역  페이지 이동

--- shoppingmall/src/components/cart/productList/OrderAmount.js
+++ shoppingmall/src/components/cart/productList/OrderAmount.js
@@ -1,12 +1,40 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 // styles
 import { BlackBtn } from "../../../style/CommonStyles";
+// components
+import Modal from "../../commom/Modal/Modal";
 
 const OrderAmount = ({ totalPrice }) => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navigateToPage = () => {
+    navigate("/order-details");
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
   return (
     <Container>
       <OrderPrice>{totalPrice.toLocaleString()}원</OrderPrice>
-      <BlackBtn padding="10px 12px">BUY NOW</BlackBtn>
+      <BlackBtn padding="10px 12px" onClick={openModal}>
+        BUY NOW
+      </BlackBtn>
+      {isOpen && (
+        <Modal
+          open={isOpen}
+          onClose={() => {
+            setIsOpen(false);
+          }}
+          title="결제금액"
+          subText={`총 결제 금액: ${totalPrice.toLocaleString()}원, 결제하시겠습니까?`}
+          navigateToPage={navigateToPage}
+        />
+      )}
     </Container>
   );
 };
