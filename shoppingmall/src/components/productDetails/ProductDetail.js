e14d003500c0e68ad64b4bb16b4f1433dff7bd82
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-05-27 12:18:26 +0900

    상품 정보 내역 ProductInfo 컴포넌트로 분리

--- shoppingmall/src/components/productDetails/ProductDetail.js
+++ shoppingmall/src/components/productDetails/ProductDetail.js
@@ -3,14 +3,11 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 // svg
-// import { FiCircle } from "react-icons/fi";
 import leftarrow from "../../assets/leftarrow.svg";
-import profileimage from "../../assets/profileimage.svg";
-import unlike from "../../assets/unlike.svg";
-// import rightarrow from "../../assets/rightarrow.svg";
 
 // component
 import ProductCarousel from './ProductCarousel';
+import ProductInfo from './ProductInfo';
 
 
 const ProductDetail = () => {
@@ -28,24 +25,7 @@ const ProductDetail = () => {
                     <ProductTitle>상품 정보</ProductTitle>
                 </Header>
                 <ProductCarousel/>
-                <ContentWrapper>
-                    <UserInfo>
-                        <UserImage src={profileimage} alt="personIcon"/>
-                        <UserName>
-                            사용자이름
-                        </UserName>
-                        <HeartIcon src={unlike} alt="좋아요"/>
-                    </UserInfo>
-                    <BorderLine></BorderLine>
-                    <Content>
-                        <ProductName>아디다스 펜츠</ProductName>
-                        <ProductPrice>56,000원</ProductPrice>
-                        <Description>
-                            상품설명
-                        </Description>
-                        <Cagetgory>#옵션 #옵션</Cagetgory>
-                    </Content>
-                </ContentWrapper>
+                <ProductInfo/>
             </Container>
             <CartButton>장바구니 담기</CartButton>
         </ProductDetailWrapper>
@@ -93,85 +73,6 @@ const ProductTitle = styled.div`
     margin-top: 3px;
     font-weight: bold;
     font-size: 24px;
-`;
-
-const ContentWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`;
-
-const UserInfo = styled.div`
-    display: flex;
-    justify-content: start;
-    width: 400px;
-    height: 55px;
-    margin-top: 25px;
-    marigin-bottom: 14px;
-`;
-
-const UserImage = styled.img`
-    display: inline-block;
-    width: 50px;
-    height: 50px;
-    margin-right: 12px;
-`;
-
-const UserName = styled.div`
-    width: 756px;
-    height: 19px;
-    margin-top: 15px;
-    margin-bottom: 30px;
-    font-size: 16px;
-    font-weight: bold;
-`;
-
-const HeartIcon = styled.img`
-    width: 20px;
-    height: 18.3px;
-    margin-top: 11px;
-    cursor: pointer;
-`;
-
-const BorderLine = styled.div`
-    width: 400px;
-    border-bottom: 1px solid #D1D4D8;
-    margin-top: 14px;
-    margin-bottom: 26px;
-`;
-
-const Content = styled.div`
-    width: 400px;
-    hegith: 100%;
-`;
-
-const ProductName = styled.div`
-    width: 150px;
-    height: 19px;
-    font-size: 16px;
-`;
-
-const ProductPrice = styled.div`
-    width: 94px;
-    height: 24px;
-    margin-top: 6px;
-    font-size: 20px;
-    font-weight: bold;
-`;
-
-const Description = styled.div`
-    width: 400px;
-    height: 19px;
-    margin-top: 16px;
-    fonst-size: 16px;
-`;
-
-const Cagetgory = styled.div`
-    width: 100px;
-    height: 18px;
-    margin-top: 75px;
-    font-size: 15px;
-    color: #858585;
 `;
 
 const CartButton = styled.button`
