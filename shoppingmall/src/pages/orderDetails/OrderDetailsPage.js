daba01bf8235aa144bf8c4b484f7e7736a2b3c9a
ujeans <dbwlsxkal95@gmail.com>
2024-05-28 15:33:25 +0900

    feat(feature/ujin/#67): 주문내역 get 요청을 통해 내역 불러오기

--- shoppingmall/src/pages/orderDetails/OrderDetailsPage.js
+++ shoppingmall/src/pages/orderDetails/OrderDetailsPage.js
@@ -1,20 +1,48 @@
 import { useLocation } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+
 // components
 import ContentLayout from "../../components/commom/ContentLayout";
 // styles
 import EmptyContentLayout from "../../components/commom/EmptyContentLayout";
 import OrderDetailsList from "../../components/orderDetails/OrderDetailsList";
+// hooks
+import useFetchData from "../../hooks/useFetchData";
+// styles
+import { LoadingSpinner } from "../../style/CommonStyles";
 
 const OrderDetailsPage = () => {
+  const userId = 15;
+  const orderUrl = `${process.env.REACT_APP_API_URL}/order/${userId}`;
+
+  const { data: orderItems, loading, error } = useFetchData(orderUrl);
+
   const location = useLocation();
-  const { orderItems } = location.state || { orderItems: [] };
+  const { orderItems: stateOrderItems = [] } = location.state || {
+    orderItems: [],
+  };
+
+  if (loading) {
+    return (
+      <LoadingSpinner>
+        <ClipLoader size={150} />
+      </LoadingSpinner>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const itemsToDisplay =
+    stateOrderItems.length > 0 ? stateOrderItems : orderItems;
 
   return (
     <ContentLayout title={"주문내역"} width="1000px">
-      {orderItems.length === 0 ? (
+      {itemsToDisplay.length === 0 ? (
         <EmptyContentLayout content={"주문내역에 상품이 없습니다."} />
       ) : (
-        <OrderDetailsList orderItems={orderItems} />
+        <OrderDetailsList orderItems={itemsToDisplay} />
       )}
     </ContentLayout>
   );
